0b9f3a796953a09400ba8fcc2663bd1ae6c24e3e
hagaitski <i.haga629@gmail.com>
2025-04-27 20:18:31 +0900

    feat: add get_next_task tool to retrieve the next task in a recipe

--- index.ts
+++ index.ts
@@ -10,6 +10,55 @@ const server = new McpServer({
 });
 
 server.tool(
+  "get_next_task",
+  "次のタスクを取得する",
+  {
+    cookbookPath: z.string().describe("cookbookのディレクトリの絶対パス"),
+    recipeName: z.string().describe("レシピ名"),
+    currentTaskId: z.string().nullable().describe("直前に完了したタスクid レシピを始めた時点ではnull"),
+  },
+  async ({ recipeName, cookbookPath, currentTaskId: currentTaskName }) => {
+    const recipePath = path.join(cookbookPath, recipeName);
+    const tasks = await fs.readdir(recipePath, { withFileTypes: true });
+    const taskSortedByName = tasks
+      .filter(entry => entry.isFile())
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    const currentTaskIndex = taskSortedByName.findIndex(task => task.name === currentTaskName);
+
+    const nextTaskIndex = currentTaskIndex + 1;
+    const nextTask = taskSortedByName[nextTaskIndex];
+    if (!nextTask) {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "次のタスクはありません。"
+          }
+        ]
+      };
+    }
+    const nextTaskPath = path.join(recipePath, nextTask.name);
+    const nextTaskContent = await fs.readFile(nextTaskPath, "utf-8");
+    return {
+      content: [
+        {
+          type: "text",
+          text: `
+          タスクid:
+          ${nextTask.name}
+          次のタスク: 
+          ${nextTaskContent}
+
+          完了した場合、次のタスクを get_next_task で取得してください。
+          `
+        }
+      ]
+    };
+  }
+)
+
+server.tool(
   "get_recipes",
   "レシピ一覧を取得する",
   {
